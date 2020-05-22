948effcf74820af9c760de4194681579050e41c5
Maria <43845187+princessMary@users.noreply.github.com>
2020-05-22 11:27:12 +0300

    Update my_js.js

--- my_js.js
+++ my_js.js
@@ -44,20 +44,20 @@ function get_keyword(){
 function getList(){
   var key = get_keyword();
   var word = key.toLowerCase();
-  if (word == "coding" || word== "code"|| word== "programming"){
-    word += word;
-    word += ", programming";
-  }
-
-  if (word.includes(",")){
-    var partsOfWord = word.split(', ');
-    for (var w = 0; w < N; w++) {
-      if (partsOfWord[w] == "coding" || partsOfWord[w]== "code" || partsOfWord[w]== "programming"){
-        partsOfWord.push("coding");
-        partsOfWord.push("programming");
-      }
-    }
-  }
+  // if (word == "coding" || word== "code"|| word== "programming"){
+  //   word += word;
+  //   word += ", programming";
+  // }
+  //
+  // if (word.includes(",")){
+  //   var partsOfWord = word.split(', ');
+  //   for (var w = 0; w < N; w++) {
+  //     if (partsOfWord[w] == "coding" || partsOfWord[w]== "code" || partsOfWord[w]== "programming"){
+  //       partsOfWord.push("coding");
+  //       partsOfWord.push("programming");
+  //     }
+  //   }
+  // }
 
   var listed_title = [];
   var listed_text = [];
@@ -109,14 +109,16 @@ function getList(){
       var words = extension(word);
       TF += description.split(words).length - 1;
       TF += learn.split(words).length - 1;
-      TF += (title.split(words).length - 1)*10;
+      // TF += (title.split(words).length - 1)*10;
+      TF += title.split(words).length - 1;
     }
     else {
       for (var h = 0; h < partsOfWord.length; h++) {
         var words = extension(partsOfWord[h]);
         TF += description.split(words).length - 1;
         TF += learn.split(words).length - 1;
-        TF += (title.split(words).length - 1)*10;
+        // TF += (title.split(words).length - 1)*10;
+        TF += title.split(words).length - 1;
       }
     }
 
@@ -266,215 +268,215 @@ function intersection() {
   return result;
 }
 
+//
+// function filter_list(course_dict){
+//
+//   // var course_dict = getList();
+//   var listed_text = [];
+//   var beginner_courses = [];
+//   var final_list = [];
+//   // var creat = getList_append("creativ");
+//   // var sustainab = getList_append("sustainable");
+//   // var entrepren = getList_append("entrepren");
+//
+//   var listed_text0 = [];
+//   var listed_text1 = [];
+//
+//
+//   var x = document.getElementsByName("animal");
+//   var myyt = false;
+//   for (var k = 0; k < x.length; k++) {
+//     if (x[k].checked == true){
+//       myyt = true;
+//     }
+//   }
+//   if (myyt == false){
+//     listed_text0 = course_dict;
+//   }
+//   else {
+//     for (var k = 0; k < x.length; k++) {
+//       if (k==0 && x[k].checked == true){
+//         for(var i = 0;i < course_dict.length; i++){
+//           if (course_dict[i]["pre"].includes("Ei esitietovaatimuksia") || course_dict[i]["pre"].includes("No prerequisite") || course_dict[i]["pre"].includes("None") || course_dict[i]["pre"].includes("No ") && (!(course_dict[i]["pre"].includes("sufficient")))){
+//             beginner_courses.push(course_dict[i]);
+//           }
+//         }
+//       }
+//       else if (k==1 && x[k].checked == true){
+//         for(var i = 0;i < course_dict.length; i++){
+//           if (course_dict[i]["kieli"].startsWith('En')){
+//             listed_text1.push(course_dict[i]);
+//           }
+//         }
+//       }
+//       // else if (k==2 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["credit"].includes("5")){
+//       //       listed_text2.push(course_dict[i]);
+//       //     }
+//       //   }
+//       // }
+//       // else if (k==3 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["credit"].startsWith("1 ") || course_dict[i]["credit"].startsWith("2") || course_dict[i]["credit"].startsWith("3")|| course_dict[i]["credit"].startsWith("4")){
+//       //       listed_text3.push(course_dict[i]);
+//       //     }
+//       //   }
+//       // }
+//       // else if (k==4 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["credit"].startsWith("6") || course_dict[i]["credit"].startsWith("7") || course_dict[i]["credit"].startsWith("8")|| course_dict[i]["credit"].includes("10")){
+//       //       listed_text4.push(course_dict[i]);
+//       //     }
+//       //   }
+//       // }
+//       // else if (k==5 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["level"].includes("Bachelor") || course_dict[i]["level"].includes("Basic") || course_dict[i]["level"].includes("bachelor")|| course_dict[i]["level"].includes("Kandidaattitaso")|| course_dict[i]["level"].includes("Kandidaattiopinnot")|| course_dict[i]["level"].includes("kandidaattiopinnot")){
+//       //       listed_text5.push(course_dict[i]);
+//       //     }
+//       //   }
+//       // }
+//       // else if (k==6 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["level"].includes("Master") || course_dict[i]["level"].includes("Advanced")|| course_dict[i]["level"].includes("advanced")|| course_dict[i]["level"].includes("master")|| course_dict[i]["level"].includes("MSc")|| course_dict[i]["level"].includes("M.Sc.")|| course_dict[i]["level"].includes("syventävät")){
+//       //       listed_text6.push(course_dict[i]);
+//       //     }
+//       //   }
+//       // }
+//       // else if (k==7 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //       if (course_dict[i]["name"].includes('Doctoral') || course_dict[i]["level"].includes('octoral') || course_dict[i]["level"].includes('post') || course_dict[i]["level"].includes('jatko')){
+//       //       listed_text7.push(course_dict[i]);
+//       //     }
+//       //   }
+//       // }
+//       // else if (k==8 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["period"].startsWith("II") || course_dict[i]["period"].startsWith("I")){
+//       //       listed_text8.push(course_dict[i]);
+//       //     }
+//       //   }
+//       // }
+//       // else if (k==9 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["period"].startsWith("III") || course_dict[i]["period"].startsWith("IV")|| course_dict[i]["period"].startsWith("V")){
+//       //       listed_text9.push(course_dict[i]);
+//       //     }
+//       //   }
+//       // }
+//       // else if (k==10 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["period"].includes("kesä") || course_dict[i]["period"].includes("summer")|| course_dict[i]["period"].includes("Summer")){
+//       //       listed_text10.push(course_dict[i]);
+//       //     }
+//       //   }
+//       // }
+//       // else if (k==11 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["learning"].includes("creativ") || course_dict[i]["description"].includes("creativ")){
+//       //       listed_text11.push(course_dict[i]);
+//       //     }
+//       //   }
+//       //   for(var i = 0;i < 20; i++){
+//       //     listed_text11.push(creat[i]);
+//       //   }
+//       // }
+//       // else if (k==12 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["learning"].includes("sustainab") || course_dict[i]["description"].includes("sustainab")){
+//       //       listed_text12.push(course_dict[i]);
+//       //     }
+//       //   }
+//       //   for(var i = 0;i < 20; i++){
+//       //     listed_text12.push(sustainab[i]);
+//       //   }
+//       // }
+//       // else if (k==13 && x[k].checked == true){
+//       //   for(var i = 0;i < course_dict.length; i++){
+//       //     if (course_dict[i]["learning"].includes("entrepren") || course_dict[i]["description"].includes("entrepren")){
+//       //       listed_text13.push(course_dict[i]);
+//       //     }
+//       //   }
+//       //   for(var i = 0;i < 20; i++){
+//       //     listed_text13.push(entrepren[i]);
+//       //   }
+//       // }
+//
+//     }
+//   }
+//   // var definitions = ['business',"workshop","media", "mathematics",'engineering', 'management', ' art ', 'technology', 'architecture', 'history', 'communication', 'materials', 'digital', 'urban', 'energy', 'physics', 'production', 'economics', 'structures', 'marketing'];
+//   // var y = document.getElementsByName("monkey");
+//   // var kyyt = false;
+//   // var monkey_list = [];
+//   // var monkey_list0 = [];
+//   // var monkey_list1 = [];
+//   // var monkey_list2 = [];
+//   //
+//   // for (var k = 0; k < y.length; k++) {
+//   //   if (y[k].checked == true){
+//   //     kyyt = true;
+//   //   }
+//   // }
+//   // if (kyyt == false){
+//   //   monkey_list = course_dict;
+//   // }
+//   // else {
+//   //   for (var k = 0; k < y.length; k++) {
+//   //     if (y[k].checked == true){
+//   //       for (var i = 0;i < course_dict.length; i++){
+//   //         var title = course_dict[i]["learning"].toLowerCase();
+//   //         var title2 = course_dict[i]["name"].toLowerCase();
+//   //         var title3 = course_dict[i]["description"].toLowerCase();
+//   //         if (title.includes(definitions[k]) || title2.includes(definitions[k]) || title3.includes(definitions[k])){
+//   //           monkey_list1.push(course_dict[i]);
+//   //         }
+//   //       }
+//   //       monkey_list.push(monkey_list1);
+//   //     }
+//   //   }
+//   // }
+//
+//   if (beginner_courses.length > 0){
+//      final_list = beginner_courses;
+//   }
+//   else {
+//
+//     listed_text_tot.push(listed_text0);
+//     listed_text_tot.push(listed_text1);
+//     // listed_text_tot.push(listed_text2);
+//     // listed_text_tot.push(listed_text3);
+//     // listed_text_tot.push(listed_text4);
+//     // listed_text_tot.push(listed_text5);
+//     // listed_text_tot.push(listed_text6);
+//     // listed_text_tot.push(listed_text7);
+//     // listed_text_tot.push(listed_text8);
+//     // listed_text_tot.push(listed_text9);
+//     // listed_text_tot.push(listed_text10);
+//     // listed_text_tot.push(listed_text11);
+//     // listed_text_tot.push(listed_text12);
+//     // listed_text_tot.push(listed_text13);
+//
+//     for (var i = 0; i < listed_text_tot.length; i++) {
+//       if (listed_text_tot[i].length>0){
+//         listed_text_tot2.push(listed_text_tot[i]);
+//       }
+//     }
+//     // for (var i = 0; i < monkey_list.length; i++) {
+//     //   if (monkey_list[i].length>0){
+//     //     listed_text_tot2.push(monkey_list[i]);
+//     //   }
+//     // }
+//     final_list = intersection(listed_text_tot2);
+//   }
+//
+//   return final_list;
+//
+// }
 
-function filter_list(course_dict){
+function print_list(listed_text){
 
-  // var course_dict = getList();
-  var listed_text = [];
-  var beginner_courses = [];
-  var final_list = [];
-  // var creat = getList_append("creativ");
-  // var sustainab = getList_append("sustainable");
-  // var entrepren = getList_append("entrepren");
-
-  var listed_text0 = [];
-  var listed_text1 = [];
-
-
-  var x = document.getElementsByName("animal");
-  var myyt = false;
-  for (var k = 0; k < x.length; k++) {
-    if (x[k].checked == true){
-      myyt = true;
-    }
-  }
-  if (myyt == false){
-    listed_text0 = course_dict;
-  }
-  else {
-    for (var k = 0; k < x.length; k++) {
-      if (k==0 && x[k].checked == true){
-        for(var i = 0;i < course_dict.length; i++){
-          if (course_dict[i]["pre"].includes("Ei esitietovaatimuksia") || course_dict[i]["pre"].includes("No prerequisite") || course_dict[i]["pre"].includes("None") || course_dict[i]["pre"].includes("No ") && (!(course_dict[i]["pre"].includes("sufficient")))){
-            beginner_courses.push(course_dict[i]);
-          }
-        }
-      }
-      else if (k==1 && x[k].checked == true){
-        for(var i = 0;i < course_dict.length; i++){
-          if (course_dict[i]["kieli"].startsWith('En')){
-            listed_text1.push(course_dict[i]);
-          }
-        }
-      }
-      // else if (k==2 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["credit"].includes("5")){
-      //       listed_text2.push(course_dict[i]);
-      //     }
-      //   }
-      // }
-      // else if (k==3 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["credit"].startsWith("1 ") || course_dict[i]["credit"].startsWith("2") || course_dict[i]["credit"].startsWith("3")|| course_dict[i]["credit"].startsWith("4")){
-      //       listed_text3.push(course_dict[i]);
-      //     }
-      //   }
-      // }
-      // else if (k==4 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["credit"].startsWith("6") || course_dict[i]["credit"].startsWith("7") || course_dict[i]["credit"].startsWith("8")|| course_dict[i]["credit"].includes("10")){
-      //       listed_text4.push(course_dict[i]);
-      //     }
-      //   }
-      // }
-      // else if (k==5 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["level"].includes("Bachelor") || course_dict[i]["level"].includes("Basic") || course_dict[i]["level"].includes("bachelor")|| course_dict[i]["level"].includes("Kandidaattitaso")|| course_dict[i]["level"].includes("Kandidaattiopinnot")|| course_dict[i]["level"].includes("kandidaattiopinnot")){
-      //       listed_text5.push(course_dict[i]);
-      //     }
-      //   }
-      // }
-      // else if (k==6 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["level"].includes("Master") || course_dict[i]["level"].includes("Advanced")|| course_dict[i]["level"].includes("advanced")|| course_dict[i]["level"].includes("master")|| course_dict[i]["level"].includes("MSc")|| course_dict[i]["level"].includes("M.Sc.")|| course_dict[i]["level"].includes("syventävät")){
-      //       listed_text6.push(course_dict[i]);
-      //     }
-      //   }
-      // }
-      // else if (k==7 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //       if (course_dict[i]["name"].includes('Doctoral') || course_dict[i]["level"].includes('octoral') || course_dict[i]["level"].includes('post') || course_dict[i]["level"].includes('jatko')){
-      //       listed_text7.push(course_dict[i]);
-      //     }
-      //   }
-      // }
-      // else if (k==8 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["period"].startsWith("II") || course_dict[i]["period"].startsWith("I")){
-      //       listed_text8.push(course_dict[i]);
-      //     }
-      //   }
-      // }
-      // else if (k==9 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["period"].startsWith("III") || course_dict[i]["period"].startsWith("IV")|| course_dict[i]["period"].startsWith("V")){
-      //       listed_text9.push(course_dict[i]);
-      //     }
-      //   }
-      // }
-      // else if (k==10 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["period"].includes("kesä") || course_dict[i]["period"].includes("summer")|| course_dict[i]["period"].includes("Summer")){
-      //       listed_text10.push(course_dict[i]);
-      //     }
-      //   }
-      // }
-      // else if (k==11 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["learning"].includes("creativ") || course_dict[i]["description"].includes("creativ")){
-      //       listed_text11.push(course_dict[i]);
-      //     }
-      //   }
-      //   for(var i = 0;i < 20; i++){
-      //     listed_text11.push(creat[i]);
-      //   }
-      // }
-      // else if (k==12 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["learning"].includes("sustainab") || course_dict[i]["description"].includes("sustainab")){
-      //       listed_text12.push(course_dict[i]);
-      //     }
-      //   }
-      //   for(var i = 0;i < 20; i++){
-      //     listed_text12.push(sustainab[i]);
-      //   }
-      // }
-      // else if (k==13 && x[k].checked == true){
-      //   for(var i = 0;i < course_dict.length; i++){
-      //     if (course_dict[i]["learning"].includes("entrepren") || course_dict[i]["description"].includes("entrepren")){
-      //       listed_text13.push(course_dict[i]);
-      //     }
-      //   }
-      //   for(var i = 0;i < 20; i++){
-      //     listed_text13.push(entrepren[i]);
-      //   }
-      // }
-
-    }
-  }
-  // var definitions = ['business',"workshop","media", "mathematics",'engineering', 'management', ' art ', 'technology', 'architecture', 'history', 'communication', 'materials', 'digital', 'urban', 'energy', 'physics', 'production', 'economics', 'structures', 'marketing'];
-  // var y = document.getElementsByName("monkey");
-  // var kyyt = false;
-  // var monkey_list = [];
-  // var monkey_list0 = [];
-  // var monkey_list1 = [];
-  // var monkey_list2 = [];
-  //
-  // for (var k = 0; k < y.length; k++) {
-  //   if (y[k].checked == true){
-  //     kyyt = true;
-  //   }
-  // }
-  // if (kyyt == false){
-  //   monkey_list = course_dict;
-  // }
-  // else {
-  //   for (var k = 0; k < y.length; k++) {
-  //     if (y[k].checked == true){
-  //       for (var i = 0;i < course_dict.length; i++){
-  //         var title = course_dict[i]["learning"].toLowerCase();
-  //         var title2 = course_dict[i]["name"].toLowerCase();
-  //         var title3 = course_dict[i]["description"].toLowerCase();
-  //         if (title.includes(definitions[k]) || title2.includes(definitions[k]) || title3.includes(definitions[k])){
-  //           monkey_list1.push(course_dict[i]);
-  //         }
-  //       }
-  //       monkey_list.push(monkey_list1);
-  //     }
-  //   }
-  // }
-
-  if (beginner_courses.length > 0){
-     final_list = beginner_courses;
-  }
-  else {
-
-    listed_text_tot.push(listed_text0);
-    listed_text_tot.push(listed_text1);
-    // listed_text_tot.push(listed_text2);
-    // listed_text_tot.push(listed_text3);
-    // listed_text_tot.push(listed_text4);
-    // listed_text_tot.push(listed_text5);
-    // listed_text_tot.push(listed_text6);
-    // listed_text_tot.push(listed_text7);
-    // listed_text_tot.push(listed_text8);
-    // listed_text_tot.push(listed_text9);
-    // listed_text_tot.push(listed_text10);
-    // listed_text_tot.push(listed_text11);
-    // listed_text_tot.push(listed_text12);
-    // listed_text_tot.push(listed_text13);
-
-    for (var i = 0; i < listed_text_tot.length; i++) {
-      if (listed_text_tot[i].length>0){
-        listed_text_tot2.push(listed_text_tot[i]);
-      }
-    }
-    // for (var i = 0; i < monkey_list.length; i++) {
-    //   if (monkey_list[i].length>0){
-    //     listed_text_tot2.push(monkey_list[i]);
-    //   }
-    // }
-    final_list = intersection(listed_text_tot2);
-  }
-
-  return final_list;
-
-}
-
-function print_list(){
-
-  var listed_text = getList();
+  // var listed_text = getList();
   // var listed_text = filter_list(lista);
 
   var pituus = listed_text.length;
